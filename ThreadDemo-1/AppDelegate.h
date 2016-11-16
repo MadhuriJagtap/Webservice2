39ff30f9653b93e52e65fdba4182a37b2ac7c67c
MadhuriJagtap <madhuri@felix-its.com>
2016-11-16 10:08:49 +0530

    Initial Commit

---
+++ ThreadDemo-1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThreadDemo-1
+//
+//  Created by Felix-Madhuri on 16/11/16.
+//  Copyright © 2016 Felix-Madhuri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
